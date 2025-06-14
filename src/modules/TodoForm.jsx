b0f70fe0d21f584ad43da179ac633a7cb17765d4
Ilyosbek <ilyosbekibroximov23z@gmail.com>
2025-06-14 16:57:02 +0500

    finishedd

--- src/modules/TodoForm.jsx
+++ src/modules/TodoForm.jsx
@@ -15,7 +15,7 @@ const TodoForm = () => {
   }
   return (
     <form onSubmit={handleAddTodo} autoComplete='off' className='w-full min-w-[300px] max-w-[500px] flex flex-col min-[400px]:flex-row items-center justify-between p-2 min-[400px]:p-5 rounded-md bg-white mx-auto mt-10 gap-2 min-[400px]:gap-[10px]'>
-      <input required className='w-full min-[400px]:w-[78%] py-2 min-[400px]:py-3 pl-2 rounded-md border-[2px] border-slate-500 text-sm min-[400px]:text-base' name='todoInp' type="text" />
+      <input required placeholder='typing...' className='w-full min-[400px]:w-[78%] py-2 min-[400px]:py-3 pl-2 rounded-md border-[2px] border-slate-500 text-sm min-[400px]:text-base' name='todoInp' type="text" />
       <button className='w-full min-[400px]:w-[18%] p-2 min-[400px]:p-[12px] font-bold text-[16px] min-[400px]:text-[20px] text-[#0B1D51] bg-gradient-to-br from-pink-200 to-blue-200 rounded-md' type='submit'>Add</button>
     </form>
   )
